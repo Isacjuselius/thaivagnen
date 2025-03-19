15c7892d76f021d16a9e938f4c731ec4d8d39488
isacj <id23ias@cs.umu.se>
2025-03-19 10:08:31 +0100

    fadeTo() och animate()

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,3 +1,32 @@
 document.querySelector(".jsButton").addEventListener("click", function() {
     alert("Hello JavaScript");
 });
+
+$(function(){
+    $(".fade").click(function(){
+        $(".fadeTextblock1").fadeTo(2000, 0.5); // tonar textblocket till 50% under 2 sekunder
+        $(".fadeTextblock2").fadeTo(2000, 0.2); // tonar textblocket til 20% under 2 sekunder
+    });
+});
+
+$(function(){
+
+    $(".thaiVagnen").click(function(){
+        $(".thaiVagnen").animate({
+            width: '+=10vw', //Gör den större 
+            backgroundColor: '#a2ae8f' // Ger den en annan färg
+        }, "slow") //Sätter hur snabbt animationen sker 
+    })
+    $(".farskaVaror").click(function(){
+        $(".farskaVaror").animate({
+            width: '+=10vw',
+            backgroundColor: '#a2ae8f'
+        }, "slow")
+    })
+    $(".ersboda").click(function(){
+        $(".ersboda").animate({
+            width: '+=10vw',
+            backgroundColor: '#a2ae8f'
+        }, "slow")
+    })
+})
